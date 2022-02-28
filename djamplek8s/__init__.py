05fc12983ec47a82c5ba96628a6c4b741e52280b
Maarten <ikmaarten@gmail.com>
2022-02-28 10:48:03 +0100

    Bump version: 0.0.1 → 0.0.2

--- djamplek8s/__init__.py
+++ djamplek8s/__init__.py
@@ -4,7 +4,7 @@
 A Django example project for Kubernetes
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __title__ = "djamplek8s"
 __description__ = "Django example project for Kubernetes"
 __uri__ = "https://github.com/maartenq/djamplek8s"
